0f25d8bdab723adbdfc842ba9ecdc669b8e87268
Will <125041335+will258012@users.noreply.github.com>
2025-08-27 22:18:07 +0800

    Adapt to AlgernonCommons

--- CSLMusicMod/CSLMusicMod.cs
+++ CSLMusicMod/CSLMusicMod.cs
@@ -1,63 +1,32 @@
-using System;
-using ICities;
-using UnityEngine;
-using System.Collections;
-using System.IO;
-using System.Reflection;
+using AlgernonCommons;
+using AlgernonCommons.Patching;
+using AlgernonCommons.Translation;
 using CSLMusicMod.UI;
+using ICities;
 
 namespace CSLMusicMod
 {
     /// <summary>
     /// Main mod class
     /// </summary>
-    public class CSLMusicMod : IUserMod
+    public class CSLMusicMod : PatcherMod<SettingsUI, PatcherBase>, IUserMod
     {
         public static System.Random RANDOM = new System.Random();
-
-        public const String VersionName = "Rewrite 1.1.11.4";
-
-        private SettingsUI m_SettingsUI;
-
-        public string Name
-        {
-            get
-            {
-                return "CSL Music Mod";
-            }
-        }
-
-        public string Description
-        {
-            get
-            {
-                return "Adds custom radio stations the game";
-            }
-        }
-
-        public CSLMusicMod()
-        {
-            Debug.Log("[CSLMusic] Version " + VersionName);
-        }
-
-        public void OnSettingsUI(UIHelperBase helper)
-        {
-            m_SettingsUI = new SettingsUI();
-            m_SettingsUI.InitializeSettingsUI(helper);
-        }
+        public override string BaseName => "CSL Music Mod";
+        public string Description => Translations.Translate("MOD_DESCRIPTION");
+        public override string HarmonyID => "Will258012.CSLMusicMod";
 
         /// <summary>
         /// Logs into the debug log if enabled.
         /// </summary>
         /// <returns>The log.</returns>
         /// <param name="message">Message.</param>
-        public static void Log(object message)
-        {
-            if(ModOptions.Instance.EnableDebugInfo)
-            {
-                Debug.Log("[CSLMusicMod] " + message);
-            }
-        }
+        public static void Log(object message) => Logging.Message(message);
+
+        public override void LoadSettings() => ModOptions.Instance.LoadSettings();
+
+        public override void SaveSettings() => ModOptions.Instance.SaveSettings();
+
     }
 }
 
