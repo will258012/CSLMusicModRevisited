56399adb7c8cd8f15d5e4775cd9fa6671d4f563b
Will <125041335+will258012@users.noreply.github.com>
2025-09-13 14:48:00 +0800

    Add what's new message

--- CSLMusicMod/CSLMusicMod.cs
+++ CSLMusicMod/CSLMusicMod.cs
@@ -1,3 +1,5 @@
+using AlgernonCommons;
+using AlgernonCommons.Notifications;
 using AlgernonCommons.Patching;
 using AlgernonCommons.Translation;
 using CSLMusicMod.UI;
@@ -14,6 +16,20 @@ namespace CSLMusicMod
         public override string BaseName => "CSL Music Mod Revisited";
         public string Description => Translations.Translate("MOD_DESCRIPTION");
         public override string HarmonyID => "Will258012.CSLMusicMod";
+        public override WhatsNewMessage[] WhatsNewMessages => new WhatsNewMessage[]
+       {
+            new WhatsNewMessage
+            {
+                Version = AssemblyUtils.CurrentVersion,
+                MessagesAreKeys = true,
+                Messages = new string[]
+                {
+                    "WHATSNEW_L1",
+                    "WHATSNEW_L2",
+                    "WHATSNEW_L3"
+                }
+            }
+       };
         public override void LoadSettings() => ModOptions.Instance.LoadSettings();
         public override void SaveSettings() => ModOptions.Instance.SaveSettings();
 
