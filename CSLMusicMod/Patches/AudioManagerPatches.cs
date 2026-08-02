812e452dc1cac9bbe86cfe86d20c10178669d0ea
Will <125041335+will258012@users.noreply.github.com>
2026-08-02 14:49:45 +0800

    Refactor audio patches to use transpilers
    - Replace Prefix patches with Transpiler patches for AudioManager methods, providing better IL-level control.
    - Simplify QueueBroadcastPatch to just check ModOptions.
    - Add FilterDisallowedContent helper to remove restricted radio content, and implement CollectAvailableRadioContentInfo fallback to try alternative content types when the requested type is unavailable.

--- CSLMusicMod/Patches/AudioManagerPatches.cs
+++ CSLMusicMod/Patches/AudioManagerPatches.cs
@@ -1,135 +1,111 @@
-﻿using ColossalFramework;
+﻿using AlgernonCommons;
 using CSLMusicMod.Helpers;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
-using System.Threading;
-using UnityEngine;
+using System.Reflection.Emit;
 
 namespace CSLMusicMod.Patches
 {
     [HarmonyPatch]
     public class AudioManagerPatches
     {
-        /// <summary>
-        /// Allows custom playback of broadcasts.
-        /// </summary>
-        /// <param name="info">The content to be played</param>
         [HarmonyPatch(typeof(AudioManager), nameof(AudioManager.QueueBroadcast))]
         [HarmonyPrefix]
-        [ReflectionHelper.UsedReflection]
-        public static bool QueueBroadcastPatch(AudioManager __instance, RadioContentInfo info)
-        {
-            if (!ModOptions.Instance.AllowContentBroadcast)
-                return false;
+        public static bool QueueBroadcastPatch(AudioManager __instance, RadioContentInfo info) => ModOptions.Instance.AllowContentBroadcast;
 
-            var broadcastQueue = ReflectionHelper.GetPrivateField<FastList<RadioContentInfo>>(__instance, "m_broadcastQueue"); //Why does CO make everything private, so you can't access it ??
-
-            while (!Monitor.TryEnter(broadcastQueue, SimulationManager.SYNCHRONIZE_TIMEOUT))
-            {
-            }
-            try
-            {
-                if (broadcastQueue.m_size < 5)
-                {
-                    for (int i = 0; i < broadcastQueue.m_size; i++)
-                    {
-                        if (broadcastQueue.m_buffer[i] == info)
-                        {
-                            return false;
-                        }
-                    }
-                    broadcastQueue.Add(info);
-                }
-            }
-            finally
-            {
-                Monitor.Exit(broadcastQueue);
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// Allows restriction of content to specific songs
-        /// </summary>
-        /// <returns>The collect radio content info.</returns>
-        /// <param name="type">Type.</param>
-        /// <param name="channel">Channel.</param>
         [HarmonyPatch(typeof(AudioManager), nameof(AudioManager.CollectRadioContentInfo))]
-        [HarmonyPrefix]
+        [HarmonyTranspiler]
         [ReflectionHelper.UsedReflection]
-        public static bool CollectRadioContentInfoPatch(AudioManager __instance, RadioContentInfo.ContentType type, RadioChannelInfo channel, ref FastList<ushort> __result)
+        public static IEnumerable<CodeInstruction> CollectRadioContentInfoPatch(IEnumerable<CodeInstruction> instructions)
         {
-            //Logging.Message("[Internal] Rebuilding the radio content of channel " + channel.GetLocalizedTitle());
-            var traverse = new Traverse(__instance);
-            // CO makes some things public and other things private. This is completely insane.
-            var m_tempRadioContentBuffer = traverse.Field("m_tempRadioContentBuffer").GetValue<FastList<ushort>>();
-            var m_radioContentTable = traverse.Field("m_radioContentTable").GetValue<FastList<ushort>[]>();
+            var codes = new List<CodeInstruction>(instructions);
+            var resultField = AccessTools.Field(typeof(AudioManager), "m_tempRadioContentBuffer");
+            var filterMethod = AccessTools.Method(typeof(AudioManagerPatches), nameof(FilterDisallowedContent));
 
-            m_tempRadioContentBuffer.Clear();
-
-            if (m_radioContentTable == null)
+            for (int i = codes.Count - 2; i >= 0; --i)
             {
-                // Let's all sing the "Expensive Song!" ♬Expensive, Expensive♬ ♩OMG it's so expensive♩ (Rest of lyrics didn't load, yet)
-                traverse.Method("RefreshRadioContentTable").GetValue();
-                m_radioContentTable = ReflectionHelper.GetPrivateField<FastList<ushort>[]>(__instance, "m_radioContentTable");
-            }
-            // Get the allowed radio content
-            HashSet<RadioContentInfo> disallowed_content = null;
-            if (channel != null)
-            {
-                RadioContentWatcher.DisallowedContentsCache.TryGetValue(channel, out disallowed_content);
-            }
-
-            //Debug.Log("[update]" + channel.GetLocalizedTitle() + " | " + allowed_content);
-            /*if(allowed_content == null || allowed_content.Count == 0)
-            {
-                Debug.Log(channel.GetLocalizedTitle() + ": All content enabled!");
-            }*/
-
-            int prefabDataIndex = channel.m_prefabDataIndex;
-            if (prefabDataIndex != -1)
-            {
-                int num = (int)(prefabDataIndex * 5 + type);
-                if (num < m_radioContentTable.Length)
+                if (codes[i].opcode == OpCodes.Ldfld && Equals(codes[i].operand, resultField) && codes[i + 1].opcode == OpCodes.Ret)
                 {
-                    FastList<ushort> fastList = m_radioContentTable[num];
-                    if (fastList != null)
-                    {
-                        for (int i = 0; i < fastList.m_size; i++)
-                        {
-                            ushort num2 = fastList.m_buffer[i];
-                            RadioContentInfo prefab = PrefabCollection<RadioContentInfo>.GetPrefab(num2);
-
-                            if (prefab != null && Singleton<UnlockManager>.instance.Unlocked(prefab.m_UnlockMilestone))
-                            {
-                                // Filter only content info that should be kept
-                                if (disallowed_content == null || disallowed_content.Count == 0 || !disallowed_content.Contains(prefab))
-                                {
-                                    prefab.m_cooldown = 1000000;
-                                    m_tempRadioContentBuffer.Add(num2);
-                                }
-                            }
-                        }
-                    }
+                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldarg_2));
+                    codes.Insert(i + 2, new CodeInstruction(OpCodes.Call, filterMethod));
+                    return codes;
                 }
             }
 
-            for (int j = 0; j < __instance.m_radioContents.m_size; j++)
-            {
-                RadioContentData.Flags flags = __instance.m_radioContents.m_buffer[j].m_flags;
-                if ((flags & RadioContentData.Flags.Created) != RadioContentData.Flags.None)
-                {
-                    RadioContentInfo info = __instance.m_radioContents.m_buffer[j].Info;
-                    if (info != null)
-                    {
-                        info.m_cooldown = Mathf.Min(info.m_cooldown, __instance.m_radioContents.m_buffer[j].m_cooldown);
-                    }
-                }
-            }
-
-            __result = m_tempRadioContentBuffer;
-            return false;
+            throw new InvalidOperationException("Failed to patch AudioManager.CollectRadioContentInfo");
         }
+
+        private static FastList<ushort> FilterDisallowedContent(FastList<ushort> contents, RadioChannelInfo channel)
+        {
+            if (contents == null || channel == null ||
+                !RadioContentWatcher.DisallowedContentsCache.TryGetValue(channel, out var disallowed) || disallowed.Count == 0)
+                return contents;
+
+            for (int i = contents.m_size - 1; i >= 0; --i)
+            {
+                var content = PrefabCollection<RadioContentInfo>.GetPrefab(contents.m_buffer[i]);
+                if (content != null && disallowed.Contains(content))
+                    contents.RemoveAt(i);
+            }
+
+            return contents;
+        }
+
+        [HarmonyPatch(typeof(RadioChannelData), "FindNextContentInfo")]
+        [HarmonyTranspiler]
+        [ReflectionHelper.UsedReflection]
+        public static IEnumerable<CodeInstruction> FindNextContentInfoPatch(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            var collectMethod = AccessTools.Method(
+                typeof(AudioManager),
+                nameof(AudioManager.CollectRadioContentInfo),
+                new[] { typeof(RadioContentInfo.ContentType), typeof(RadioChannelInfo) });
+            var fallbackMethod = AccessTools.Method(typeof(AudioManagerPatches), nameof(CollectAvailableRadioContentInfo));
+
+            int replacements = 0;
+            foreach (var code in codes)
+            {
+                if (code.opcode == OpCodes.Callvirt && Equals(code.operand, collectMethod))
+                {
+                    code.opcode = OpCodes.Call;
+                    code.operand = fallbackMethod;
+                    ++replacements;
+                }
+            }
+
+            if (replacements != 1)
+                throw new InvalidOperationException("Failed to patch RadioChannelData.FindNextContentInfo");
+
+            return codes;
+        }
+
+        private static FastList<ushort> CollectAvailableRadioContentInfo(
+            AudioManager audioManager,
+            RadioContentInfo.ContentType requestedType,
+            RadioChannelInfo channel)
+        {
+            var contents = audioManager.CollectRadioContentInfo(requestedType, channel);
+            if (contents == null || contents.m_size != 0 || channel == null || channel.m_stateChain == null)
+                return contents;
+
+            Logging.Message("Requested ", requestedType, " is not avaliable. Falling back to other ContentType");
+
+            var attemptedTypes = new HashSet<RadioContentInfo.ContentType> { requestedType };
+            foreach (var state in channel.m_stateChain)
+            {
+                if (!attemptedTypes.Add(state.m_contentType))
+                    continue;
+
+                contents = audioManager.CollectRadioContentInfo(state.m_contentType, channel);
+                if (contents != null && contents.m_size != 0)
+                    return contents;
+            }
+
+            return contents;
+        }
+
     }
 }
 
